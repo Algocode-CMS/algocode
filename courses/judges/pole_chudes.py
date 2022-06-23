475021365262efbfcdaa743914ce85ff25cef12c
grphil <grphil@mail.ru>
2022-06-23 12:59:03 +0300

    fix performance

--- courses/judges/pole_chudes.py
+++ courses/judges/pole_chudes.py
@@ -27,11 +27,21 @@ def recalc_pole_chudes_standings(game: PoleChudesGame):
 
         let.sort(key=lambda x: x["time"])
 
+        all_guessed = list(team.guesses.order_by("id"))
+        all_letters = list(team.letters.all())
+
         for i, word in enumerate(game.words.order_by("id")):
-            guessed_letters = {x.letter for x in team.letters.filter(word_id=i)}
+            guessed_letters = set()
+
+            for letter in all_letters:
+                if letter.word_id == i:
+                    guessed_letters.add(letter.letter)
+
             guess_time = 10 ** 100
 
-            for guess in team.guesses.filter(word_id=i).order_by("id"):
+            for guess in all_guessed:
+                if guess.word_id != i:
+                    continue
                 team.score += guess.score
                 if guess.guessed:
                     guess_time = guess.time.timestamp()
